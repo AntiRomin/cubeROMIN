e907b0fa645c5557c00cb9788f3e9f261e2f25c3
Romin <413196205@qq.com>
2025-03-03 13:41:24 +0800

    新增RCC驱动

--- src/main/common/utils.h
+++ src/main/common/utils.h
@@ -13,4 +13,18 @@ static inline int popcount(unsigned x) { return __builtin_popcount(x); }
 static inline int popcount32(uint32_t x) { return __builtin_popcount(x); }
 static inline int popcount64(uint64_t x) { return __builtin_popcountll(x); }
 
+/*
+ * https://groups.google.com/forum/?hl=en#!msg/comp.lang.c/attFnqwhvGk/sGBKXvIkY3AJ
+ * Return (v ? floor(log2(v)) : 0) when 0 <= v < 1<<[8, 16, 32, 64].
+ * Inefficient algorithm, intended for compile-time constants.
+ */
+#define LOG2_8BIT(v)  (8 - 90/(((v)/4+14)|1) - 2/((v)/2+1))
+#define LOG2_16BIT(v) (8*((v)>255) + LOG2_8BIT((v) >>8*((v)>255)))
+#define LOG2_32BIT(v) (16*((v)>65535L) + LOG2_16BIT((v)*1L >>16*((v)>65535L)))
+#define LOG2_64BIT(v) \
+    (32*((v)/2L>>31 > 0) \
+     + LOG2_32BIT((v)*1L >>16*((v)/2L>>31 > 0) \
+                         >>16*((v)/2L>>31 > 0)))
+#define LOG2(v) LOG2_64BIT(v)
+
 static inline int32_t cmp32(uint32_t a, uint32_t b) { return (int32_t)(a-b); }
---
+++ src/main/drivers/rcc_types.h
@@ -0,0 +1,3 @@
+#pragma once
+
+typedef uint16_t rccPeriphTag_t;
